475043b0f54be79a543daec99991abca9c1370e9
QiTianzu <qitianzu@gmail.com>
2023-11-27 22:52:02 +0800

    Update keyget.bat
    Signed-off-by: QiTianzu <qitianzu@gmail.com>

--- keyget.bat
+++ keyget.bat
@@ -1,8 +1,9 @@
 @echo off
 
-Title PikPak_By_Shimily_Keygen
-MODE con: Cols=73 Lines=9
+title PikPak_By_Shimily_Keygen
+mode con: Cols=73 Lines=9
 
+:main
 echo.
 set /p "=机器码: " < nul
 for /f "delims=" %%i in ('python %~dp0keygen.py') do (
@@ -15,5 +16,11 @@ set /p "=%code%" < nul | clip
 echo.
 echo 注册码已经自动复制到剪贴板
 echo.
-set /p "=按任意键关闭窗口" < nul
-pause > nul
+set /p "=按 C 键继续，按 Q 键退出（等待3秒，默认继续）..." < nul
+choice /c cq /n /t 3 /d c > nul
+if %errorlevel% equ 1 (
+  cls & goto main
+)
+if %errorlevel% equ 2 (
+  exit /b
+)
